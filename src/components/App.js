50ebed2f62b145027a81b515f8c4533751c9e327
Alfred Meng <alfredfmeng@gmail.com>
2021-08-30 16:49:07 -0500

    completed SignUp component and added it to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
+import SignUp from "./SignUp";
+import { Container } from "react-bootstrap";
+
 function App() {
-  return "Hello World";
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      stye={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <SignUp />
+      </div>
+    </Container>
+  );
 }
 
 export default App;
